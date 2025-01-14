042566371c6fb67ba951c535ae0b343c72066466
1Metaboys <ceo.meta24@gmail.com>
2025-01-14 18:14:11 +0300

    Delete links.js

--- links.js
+++
@@ -1,6 +0,0 @@
-const links = {
-    screen: "https://example.com/test1",
-    voice: "https://example.com/test2",
-    sfera: "https://example.com/test3",
-    chat: "https://example.com/test4",
-};
