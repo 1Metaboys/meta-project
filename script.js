61e4946ff489d897688641e9dfe3c0d3146bac71
1Metaboys <ceo.meta24@gmail.com>
2025-01-14 18:10:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const links = {
+    screen: "https://example.com/test1",
+    voice: "https://example.com/test2",
+    sfera: "https://example.com/test3",
+    chat: "https://example.com/test4",
+};
+
 document.querySelectorAll('section').forEach(section => {
     const image = section.querySelector("img");
 
